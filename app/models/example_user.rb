7e19151b07fe23f5528f1ab0ba42f265f06dde78
brandnamewater <brandnamewater@gmail.com>
2018-08-30 04:57:55 -0400

    finishd layouts and routes

---
+++ app/models/example_user.rb
@@ -0,0 +1,24 @@
+#class User
+#  attr_accessor :name, :email
+
+#  def initialize(attributes = {})
+#    @name  = attributes[:name]
+#    @email = attributes[:email]
+#  end
+
+#  def formatted_email
+#    "#{@name} <#{@email}>"
+#  end
+
+#  def full_name
+#    "#{@name}" + " " + "<#{@lname}>"
+#  end
+
+#  def alphabetical_name
+#    "#{@lname}" + ", " + "<#{@name}>"
+#  end
+
+
+#end
+
+#end
