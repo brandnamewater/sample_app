c50ddd62388e58f1e5698c93a9c7b5db35de1ad2
brandnamewater <brandnamewater@gmail.com>
2018-09-01 05:23:46 -0400

    add user foll

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -27,7 +27,7 @@ end
   def extension_whitelist
      %w(jpg jpeg gif png)
   end
- end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
